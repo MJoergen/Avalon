03fae148267a019653448c9511f3bd2f406c6dac
Michael Jørgensen <mfj@silicom.dk>
2022-10-13 06:55:21 +0200

    avm_pause: Randomize request pause

--- avm_pause.vhd
+++ avm_pause.vhd
@@ -43,36 +43,23 @@ end entity avm_pause;
 architecture synthesis of avm_pause is
 
    signal rd_burstcount : integer range 0 to 255;
-   signal cnt : integer range 0 to G_REQ_PAUSE := 0;
+   signal cnt   : std_logic_vector(16 downto 0);
+   signal inc_s : std_logic_vector(16 downto 0);
    signal allow : std_logic;
 
    type read_resp_t is array (0 to G_RESP_PAUSE) of std_logic_vector(G_DATA_SIZE downto 0);
    signal read_resp : read_resp_t;
 
+   signal lfsr_output   : std_logic_vector(21 downto 0);
+   signal lfsr_reverse  : std_logic_vector(21 downto 0);
+   signal lfsr_random   : std_logic_vector(21 downto 0);
+   signal lfsr_update_s : std_logic;
+
 begin
 
-   ------------------
-   -- Handle request
-   ------------------
-
-   p_cnt : process (clk_i)
-   begin
-      if rising_edge(clk_i) then
-         if G_REQ_PAUSE > 0 then
-            if m_avm_waitrequest_i = '0' then
-               cnt <= (cnt + 1) mod G_REQ_PAUSE;
-
-               if s_avm_write_i = '0' and s_avm_read_i = '0' then
-                  cnt <= 1;
-               end if;
-            end if;
-
-            if rst_i = '1' then
-               cnt <= 1;
-            end if;
-         end if;
-      end if;
-   end process p_cnt;
+   ----------------------
+   -- Handle read burst
+   ---------------------
 
    p_rd_burstcount : process (clk_i)
    begin
@@ -94,7 +81,27 @@ begin
    end process p_rd_burstcount;
 
 
-   allow <= '1' when cnt /= 0 or G_REQ_PAUSE = 0 else '0';
+   ------------------------------------
+   -- Insert random pauses in requests
+   ------------------------------------
+
+   inc_s(16 downto 16-G_REQ_PAUSE+1) <= (others => '0');
+   inc_s(16-G_REQ_PAUSE downto 0) <= lfsr_random(16-G_REQ_PAUSE downto 0);
+
+   p_cnt : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         if G_REQ_PAUSE > 0 then
+            cnt <= ("0" & cnt(15 downto 0)) + inc_s;
+         end if;
+
+         if rst_i = '1' then
+            cnt <= (others => '0');
+         end if;
+      end if;
+   end process p_cnt;
+
+   allow <= not cnt(16);
 
    m_avm_write_o         <= s_avm_write_i and allow when rd_burstcount = 0 else '0';
    m_avm_read_o          <= s_avm_read_i and allow when rd_burstcount = 0 else '0';
@@ -104,6 +111,7 @@ begin
    m_avm_burstcount_o    <= s_avm_burstcount_i;
    s_avm_waitrequest_o   <= '1' when rd_burstcount /= 0 else
                              m_avm_waitrequest_i or not allow;
+
 
    -------------------
    -- Handle response
@@ -130,6 +138,34 @@ begin
 
    s_avm_readdata_o      <= read_resp(G_RESP_PAUSE)(G_DATA_SIZE-1 downto 0);
    s_avm_readdatavalid_o <= read_resp(G_RESP_PAUSE)(G_DATA_SIZE);
+
+
+   --------------------------------------
+   -- Instantiate randon number generator
+   --------------------------------------
+
+   i_lfsr : entity work.lfsr
+      generic map (
+         G_WIDTH => 22,
+         G_TAPS  => X"000000000020069E" -- See https://users.ece.cmu.edu/~koopman/lfsr/22.txt
+      )
+      port map (
+         clk_i      => clk_i,
+         rst_i      => rst_i,
+         update_i   => '1',
+         load_i     => '0',
+         load_val_i => (others => '1'),
+         output_o   => lfsr_output
+      ); -- i_lfsr
+
+   p_reverse : process(all)
+   begin
+      for i in lfsr_output'low to lfsr_output'high loop
+         lfsr_reverse(lfsr_output'high - i) <= lfsr_output(i);
+      end loop;
+   end process p_reverse;
+
+   lfsr_random <= std_logic_vector(unsigned(lfsr_output) + unsigned(lfsr_reverse));
 
 end architecture synthesis;
 
